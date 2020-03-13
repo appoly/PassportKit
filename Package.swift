ae25aa3f0252b1727e46105d84ca22b5bababc55
James Wolfe <james.wolfe@appoly.co.uk>
2020-03-13 10:33:16 +0000

    Swift package target source fix

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "PassportKit",
-            path: "Source"
+            path: "Sources"
         ),
         .testTarget(
             name: "PassportKitTests",
